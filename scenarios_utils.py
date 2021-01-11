59bbc98409ebaa35f13e052ea6a0fc6981825b54
chadi <joseph.lemaitre@epfl.ch>
2021-01-11 11:08:08 +0100

    new scenarios

--- scenarios_utils.py
+++ scenarios_utils.py
@@ -7,13 +7,13 @@ def pick_scenario(setup, scn_id):
     if setup.nnodes == 107:
         scenarios_specs = {
             #'vacctotalM': [2, 5, 10, 15, 20],
-            'vacctotalperweek': [250000, 479700, 1e6],
-            'vaccpermonthM': [1, 15], # ax.set_ylim(0.05, 0.4)
+            'newdoseperweek': [250000, 479700, 1e6, 2e6],
+            'vaccpermonthM': [1, 2, 15], # ax.set_ylim(0.05, 0.4)
             'epicourse': ['U', 'L']  # 'U'
         }
     elif setup.nnodes == 10:
         scenarios_specs = {
-            'vacctotalM': [.25, .5, 1.5],
+            'newdoseperweek': [10000, 50000],
             'vaccpermonthM': [1],
             'epicourse': ['U']  # 'U', 'L'
         }
@@ -28,19 +28,19 @@ def pick_scenario(setup, scn_id):
     #    raise ValueError("Scenario is useless")
 
     tot_pop = setup.pop_node.sum()
-    scenario = {'name': f"mr-{scn_spec['epicourse']}-R{scn_spec['vaccpermonthM']}-T{scn_spec['vacctotalperweek']}",
-                'vacctotalday': scn_spec['vacctotalperweek']/7,
+    scenario = {'name': f"mr-{scn_spec['epicourse']}-L{scn_spec['vaccpermonthM']}-S{scn_spec['newdoseperweek']}",
+                'newdoseperday': scn_spec['newdoseperweek']/7,
                 'rate_fomula': f"({scn_spec['vaccpermonthM'] * 1e6 / tot_pop / 30}*pop_nd)"
                 }
     # Build beta scenarios:
     if scn_spec['epicourse'] == 'C':
         scenario['beta_mult'] = np.ones((setup.nnodes, setup.ndays))
     elif scn_spec['epicourse'] == 'U':
-        course = scipy.interpolate.interp1d([0, setup.ndays / 2, setup.ndays], [2, .4, 2], kind='quadratic')
+        course = scipy.interpolate.interp1d([0, 50, 100], [1.3, .7, 2], kind='quadratic')
         course = course(np.arange(0, setup.ndays))
         scenario['beta_mult'] = np.ones((setup.nnodes, setup.ndays)) * course
     elif scn_spec['epicourse'] == 'L':
-        course = scipy.interpolate.interp1d([0, setup.ndays / 2, setup.ndays], [.45, .45, .45], kind='quadratic')
+        course = scipy.interpolate.interp1d([0, 50, 100], [.75, .85, .45], kind='quadratic')
         course = course(np.arange(0, setup.ndays))
         scenario['beta_mult'] = np.ones((setup.nnodes, setup.ndays)) * course
     return scenario
@@ -51,20 +51,21 @@ def build_scenario(setup, scenario):
     N = setup.ndays - 1
     control_initial = np.zeros((M, N))
     max_vacc_rate = np.zeros((M, N))
-    allocated_total = 0
     unvac_nd = np.copy(setup.pop_node)
     vacc_total = np.zeros(N)
 
+    stockpile = 0
     for k in range(N):
-        vacc_total[k] = scenario['vacctotalday']
+        vacc_total[k] = scenario['newdoseperday']
+        stockpile += vacc_total[k]
         for nd in range(M):
             pop_nd = setup.pop_node[nd]
             max_vacc_rate[nd, k] = eval(scenario['rate_fomula'])
-            if (allocated_total + max_vacc_rate[nd, k] < scenario['vacctotal']) and (
-                    unvac_nd[nd] - max_vacc_rate[nd, k] > 0):
-                control_initial[nd, k] = max_vacc_rate[nd, k]
-                allocated_total += max_vacc_rate[nd, k]
-                unvac_nd[nd] -= max_vacc_rate[nd, k]
+            to_allocate = stockpile * pop_nd / setup.pop_node.sum()
+            to_allocate = min(to_allocate, max_vacc_rate[nd, k], unvac_nd[nd] - 100)
+            control_initial[nd, k] = to_allocate
+            stockpile -= to_allocate
+            unvac_nd[nd] -= to_allocate
 
     vacc_total = np.cumsum(vacc_total)
 
