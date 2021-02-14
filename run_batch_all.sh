6add964b43f49e24bb163e8b8408651c5703c9aa
chadi <joseph.lemaitre@epfl.ch>
2021-02-14 14:53:07 +0100

    error in batch run

--- run_batch_all.sh
+++ run_batch_all.sh
@@ -1,7 +1,7 @@
 sbatch batch_scripts/run_slurm_AG_small_infection.run 
 sbatch batch_scripts/run_slurm_AG_small_death.run
 
-sbatch batch_scripts/run_slurm_AG_small_death.run
+sbatch batch_scripts/run_slurm_small.run
 sbatch batch_scripts/run_slurm_medium.run
 
 sbatch batch_scripts/run_slurm_AG_medium_infection.run
