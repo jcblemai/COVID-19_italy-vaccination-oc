24211a8f020d09ec8df051741deec1769e8d5143
chadi <joseph.lemaitre@epfl.ch>
2021-02-15 15:19:00 +0100

    change names

--- run_batch_all.sh
+++ run_batch_all.sh
@@ -1,10 +1,10 @@
-sbatch batch_scripts/run_slurm_AG_small_infection.run 
-sbatch batch_scripts/run_slurm_AG_small_death.run
+sbatch batch_scriptsAG_small_infection.run 
+sbatch batch_scripts/AG_small_death.run
 
-sbatch batch_scripts/run_slurm_small.run
-sbatch batch_scripts/run_slurm_medium.run
+sbatch batch_scripts/small.run
+sbatch batch_scripts/medium.run
 
-sbatch batch_scripts/run_slurm_AG_medium_infection.run
-sbatch batch_scripts/run_slurm_AG_medium_death.run
+sbatch batch_scripts/AG_medium_infection.run
+sbatch batch_scripts/AG_medium_death.run
 
-# sbatch batch_scripts/run_slurm_big.run
+# sbatch batch_scripts/big.run
