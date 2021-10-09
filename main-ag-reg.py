c64ace386130bc59d58b7619ae5cfc96f30a2b15
Joseph Lemaitre <jo.lemaitresamra@gmail.com>
2021-10-09 14:32:10 +0200

    rerun with new conda environment

--- main-ag-reg.py
+++ main-ag-reg.py
@@ -19,8 +19,6 @@ ocp = None
 @click.option("-s", "--scenario_id", "scn_ids", default=1, help="Index of scenario to run")
 @click.option("-n", "--nnodes", "nnodes", default=20, envvar="OCP_NNODES", help="Spatial model size to run")
 @click.option("-t", "--ndays", "ndays", default=30, envvar="OCP_NDAYS", help="Number of days to run")
-@click.option("--use_matlab", "use_matlab", envvar="OCP_MATLAB", type=bool, default=False, show_default=True,
-              help="whether to use matlab for the current run")
 @click.option("-a", "--objective", "objective", type=str, default='infection', show_default=True,
               help="Whether to use agestructured OCP")
 @click.option("-f", "--file_prefix", "file_prefix", envvar="OCP_PREFIX", type=str, default='test',
@@ -28,16 +26,16 @@ ocp = None
 @click.option("-d", "--output_directory", "outdir", envvar="OCP_OUTDIR", type=str, default='model_output_AG/',
               show_default=True, help="Where to write runs")
 @click.option("-o", "--optimize", "optimize", type=bool, default=False, show_default=True, help="Whether to optimize")
-def cli(scn_ids, nnodes, ndays, use_matlab, objective, file_prefix, outdir, optimize):
+def cli(scn_ids, nnodes, ndays, objective, file_prefix, outdir, optimize):
     if not isinstance(scn_ids, list):
         scn_ids = [int(scn_ids)]
-    return scn_ids, nnodes, ndays, use_matlab, objective, file_prefix, outdir, optimize
+    return scn_ids, nnodes, ndays, objective, file_prefix, outdir, optimize
 
 
 if __name__ == '__main__':
     # standalone_mode: so click doesn't exit, see
     # https://stackoverflow.com/questions/60319832/how-to-continue-execution-of-python-script-after-evaluating-a-click-cli-function
-    scn_ids, nnodes, ndays, use_matlab, objective, file_prefix, outdir, optimize = cli(standalone_mode=False)
+    scn_ids, nnodes, ndays, objective, file_prefix, outdir, optimize = cli(standalone_mode=False)
     os.makedirs(outdir, exist_ok=True)
 
     # All arrays here are (nnodes, ndays, (nx))
@@ -47,14 +45,9 @@ if __name__ == '__main__':
 
     assert(objective == 'infection' or objective == 'death')
 
-    if use_matlab:
-        p = COVIDParametersOCP.OCParameters(setup=setup, M=M, when=when)
-        if True:
-            with open(f'italy-data/parameters_{nnodes}_{when}.pkl', 'wb') as out:
-                pickle.dump(p, out, pickle.HIGHEST_PROTOCOL)
-    else:
-        with open(f'italy-data/parameters_{nnodes}_{when}.pkl', 'rb') as inp:
-            p = pickle.load(inp)
+    with open(f'italy-data/parameters_107_future.pkl', 'rb') as inp:
+        p = pickle.load(inp)
+
 
     file_prefix = file_prefix
 
